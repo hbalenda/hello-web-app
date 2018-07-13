e54f48629c73e2c3b569c042c71b9576bf5374a7
Jay <jay@jansheski.com>
2018-07-13 14:52:11 -0700

    Add psycopg2

--- ttd/settings.py
+++ ttd/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'ttd.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ttd',
+        'USER': 'bee',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
