d53d456070d47ed2a4e8961b7923dc4d01f1e3bb
Hannah Balenda <hviolet@thestranger.com>
2018-07-23 11:51:08 -0700

    Browse events by name

--- events/views.py
+++ events/views.py
@@ -3,7 +3,7 @@ from django.template.defaultfilters import slugify
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
 from events.forms import EventForm
-from events.models import Event
+from events.models import Event, User
 
 # Create your views here.
 def index(request):
@@ -67,5 +67,28 @@ def create_event(request):
     'form': form,
   })
 
-def account(request):
-  return render(request, 'registration/account.html')
+def browse_by_name(request, initial=None):
+  if initial:
+    events = Event.objects.filter(
+      name__istartswith=initial).order_by('name')
+  else:
+    events = Event.objects.all().order_by('name')
+  initialLetters = []
+  print(events)
+  for event in events:
+    name = event.name.lower().split()[0]
+    initialLetters.append(name[0])
+  initialLetters = "".join(sorted(list(set(initialLetters))))
+  print(initialLetters)
+  return render(request, 'search/search.html', {
+    'events': events,
+    'initialLetters': initialLetters,
+    'initial': initial,
+  })
+
+def account(request, username):
+  user = User.objects.get(username=username)
+  events = Event.objects.filter(user=user)
+  return render(request, 'registration/account.html', {
+    'events': events
+  })
--- ttd/urls.py
+++ ttd/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 from django.contrib.auth.views import (
   password_reset,
   password_reset_done,
@@ -21,15 +21,25 @@ urlpatterns = [
   path('contact/',
       TemplateView.as_view(template_name='contact.html'),
       name='contact'),
+  # event URls
+  path('browse/', RedirectView.as_view(
+    pattern_name='browse', permanent=True)),
+  path('browse/name/', views.browse_by_name, name='browse'),
+  path('browse/name/<initial>/', views.browse_by_name, name='browse_by_name'),
+  path('events/', RedirectView.as_view(
+    pattern_name='browse', permanent=True)),
   path('events/<slug>/', views.event_detail,
     name='event_detail'),
   path('events/<slug>/edit',
     views.edit_event, name='edit_event'),
-  path('accounts/account/', views.account, name="account"),
+  # account
+  path('accounts/', include('registration.backends.simple.urls')),
+  path('accounts/account/<username>', views.account, name="account"),
   path('accounts/register/', MyRegistrationView.as_view(),
     name='registration_register'),
   path('accounts/create_event/', views.create_event,
     name='registration_create_event'),
+  # password reset URLs
   path('accounts/password/reset/', password_reset,
     {'template_name': 'registration/password_reset_form.html'},
     name="password_reset"),
@@ -48,6 +58,6 @@ urlpatterns = [
   path('accounts/password/change/done', password_change_done,
     {'template_name': 'registration/password_change_done.html'},
     name="password_change_done"),
-  path('accounts/', include('registration.backends.simple.urls')),
+  # admin
   path('admin/', admin.site.urls),
 ]
