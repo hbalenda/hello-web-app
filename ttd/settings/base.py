2c91f2575509d2038847c06cf73737776860e659
Hannah Balenda <hviolet@thestranger.com>
2018-07-25 17:34:13 -0700

    Add whitenoise

--- ttd/settings/base.py
+++ ttd/settings/base.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -120,7 +121,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-print(STATICFILES_DIRS)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Config for registration
 ACCOUNT_ACTIVATION_DAYS = 7
